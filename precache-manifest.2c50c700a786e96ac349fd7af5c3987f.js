62efeaf64178986e9248b024bb943a7a3809afca
Manuver Gujjar <Manvvergujjar@gmail.com>
2020-07-28 11:33:53 +0530

    Updates

---
+++ precache-manifest.2c50c700a786e96ac349fd7af5c3987f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f45409967af1f73386f7444eb3be8cd8",
+    "url": "/manuvergujjar.github.io/index.html"
+  },
+  {
+    "revision": "f03260e45a30a7083771",
+    "url": "/manuvergujjar.github.io/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "7ac0908ffdc144e70e6c",
+    "url": "/manuvergujjar.github.io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f03260e45a30a7083771",
+    "url": "/manuvergujjar.github.io/static/js/2.534a764b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/manuvergujjar.github.io/static/js/2.534a764b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ac0908ffdc144e70e6c",
+    "url": "/manuvergujjar.github.io/static/js/main.61f4bda5.chunk.js"
+  },
+  {
+    "revision": "660adc45f0b97d963271",
+    "url": "/manuvergujjar.github.io/static/js/runtime-main.c0d7a902.js"
+  }
+]);
