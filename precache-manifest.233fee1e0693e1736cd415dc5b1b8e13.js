c417e35ec40926e4a340980ed7176524672f911d
Manuver Gujjar <Manvvergujjar@gmail.com>
2020-07-28 12:14:33 +0530

    updated fixed not showing in gh-pages

---
+++ precache-manifest.233fee1e0693e1736cd415dc5b1b8e13.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59ce94622e76528282e33ca6278e6aef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f03260e45a30a7083771",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "6fb2e88bb15a283651e6",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f03260e45a30a7083771",
+    "url": "/static/js/2.534a764b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.534a764b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fb2e88bb15a283651e6",
+    "url": "/static/js/main.920bcfdf.chunk.js"
+  },
+  {
+    "revision": "ec9875811aa0f00c2e31",
+    "url": "/static/js/runtime-main.92ee656d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/manuvergujjar.github.io/precache-manifest.f236922c551f0d15368a073d5ca5ed85.js"
+  "/precache-manifest.233fee1e0693e1736cd415dc5b1b8e13.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/manuvergujjar.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
